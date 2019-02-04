0c64835e86c0c32776e1cbccb0c41b549a7508ac
waanee kim (호이호이) <ksw654@gmail.com>
2019-02-04 20:27:04 +0900

    todo List 로컬스토리지의 데이터를 리스트에 가져와서 출력.

--- 김성완/src/components/TodoList.vue
+++ 김성완/src/components/TodoList.vue
@@ -1,10 +1,30 @@
 <template>
-    <div>todo List</div>
+    <section>
+        <ul>
+            <li v-for="todoItem in todoItems" v-bind:key="todoItem">
+                {{todoItem}}
+            </li>
+        </ul>
+    </section>
 </template>
 
 <script>
 export default {
-
+    data(){
+        return{
+            todoItems:[]
+        }
+    },
+    created() {
+        if(localStorage.length > 0){
+            for(var i=0; i<localStorage.length; i++ ){
+                if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
+                    this.todoItems.push(localStorage.key(i));
+                }
+            }
+        }
+        console.log(localStorage.key(0));
+    },
 }
 </script>
 
