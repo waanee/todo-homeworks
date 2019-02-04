3d103e49578efb5f4f5c425c6cf40c740efd2536
waanee kim (호이호이) <ksw654@gmail.com>
2019-02-04 19:11:47 +0900

    컴포넌트들 임포트

--- 김성완/src/App.vue
+++ 김성완/src/App.vue
@@ -1,11 +1,25 @@
 <template>
   <div id="app">
+    <todo-header></todo-header>
+    <todo-input></todo-input>
+    <todo-list></todo-list>
+    <todo-footer></todo-footer>
   </div>
 </template>
 
 <script>
-export default {
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
 
+export default {
+  components:{
+    TodoHeader,
+    TodoInput,
+    TodoList,
+    TodoFooter
+  }
 }
 </script>
 
