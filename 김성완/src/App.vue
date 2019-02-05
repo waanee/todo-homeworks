d5f10298cd5ddf6b629a9812162326b8d0de4032
waanee kim (호이호이) <ksw654@gmail.com>
2019-02-05 22:26:10 +0900

    App.vue / TodoList.vue 리펙토리

--- 김성완/src/App.vue
+++ 김성완/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <todo-header></todo-header>
     <todo-input v-on:addTodoItem="addOneItem"></todo-input>
-    <todo-list v-bind:propsdata="todoItems"></todo-list>
+    <todo-list v-bind:propsdata="todoItems" v-on:removeItem="removeOneItem"></todo-list>
     <todo-footer></todo-footer>
   </div>
 </template>
@@ -24,6 +24,10 @@ export default {
       var obj = {completed:false, item:todoItem};
       localStorage.setItem(todoItem, JSON.stringify(obj));
       this.todoItems.push(obj);
+    },
+    removeOneItem(todoItem, index){
+      localStorage.removeItem(todoItem.item);
+      this.todoItems.splice(index, 1);
     }
   },
   created() {
--- 김성완/src/components/TodoList.vue
+++ 김성완/src/components/TodoList.vue
@@ -17,8 +17,7 @@ export default {
     props: ['propsdata'],
     methods: {
         removeTodo(todoItem, index){
-            localStorage.removeItem(todoItem.item);
-            this.todoItems.splice(index, 1);
+            this.$emit('removeItem', todoItem, index);
         },
         toggleComplete(todoItem, index){
             todoItem.completed = !todoItem.completed;
